6628b1c652a5be595b206d7331fd1db4acfafa0e
WiMemory <896567891@qq.com>
2020-04-17 22:52:20 +0800

    completed create-class and join-class pages

--- yunzaodao-android/src/app/routes/home/class/create-class/create-class.page.ts
+++ yunzaodao-android/src/app/routes/home/class/create-class/create-class.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonService } from 'src/app/shared/services/common.service';
+import { ToastController, AlertController } from '@ionic/angular';
+import { LocalStorageService, USER_KEY } from 'src/app/shared/services/local-storage.service';
 
 @Component({
   selector: 'app-create-class',
@@ -7,9 +11,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateClassPage implements OnInit {
 
-  constructor() { }
+  classInfo = {
+    'name': '',
+    'class': '',
+    'semester': '',
+    'school': '',
+    'college': ''
+  }
+
+  constructor(private router: Router, private httpService:CommonService, private toastCtrl: ToastController, private alertCtrl: AlertController, private localStorageService: LocalStorageService) { }
 
   ngOnInit() {
+  }
+
+  async create() {
+    let info = ''
+    for(let item in this.classInfo){
+      if(!this.classInfo[item]) {
+        switch(item){
+          case 'name':
+            info = '课程'
+            break
+          case 'class':
+            info = '班级'
+            break
+          case 'semester':
+            info = '学期'
+            break
+          case 'school':
+            info = '学校'
+            break
+          case 'college':
+            info = '院系'
+            break
+        }
+        const toast = await this.toastCtrl.create({
+          message: info + ' 不能为空',
+          duration: 3000
+        })
+        toast.present()
+        return
+      }
+    }
+    const api = '/class/create'
+    this.classInfo['phone'] = this.localStorageService.get(USER_KEY,'').phone
+    this.httpService.ajaxPost(api, this.classInfo).then(async (res:any)=>{
+      if(res.code == 200){
+        const alert = await this.alertCtrl.create({
+          header: '提示',
+          backdropDismiss: false,
+          message: '班课 '+this.classInfo.name+' 已创建',
+          buttons: [{
+            text: '确定',
+            handler: () => {
+              this.router.navigateByUrl('home/class')
+            }
+          }]
+        })
+        alert.present()
+      }
+    })
   }
 
 }
--- yunzaodao-android/src/app/routes/home/class/join-class/join-class.page.ts
+++ yunzaodao-android/src/app/routes/home/class/join-class/join-class.page.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { ToastController } from '@ionic/angular';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ToastController, IonSlides, AlertController } from '@ionic/angular';
+import { CommonService } from 'src/app/shared/services/common.service';
+import { LocalStorageService, USER_KEY } from 'src/app/shared/services/local-storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-join-class',
@@ -8,16 +11,42 @@ import { ToastController } from '@ionic/angular';
 })
 export class JoinClassPage implements OnInit {
 
-  classNumber = ''
+  classNumber = '1314'
 
-  constructor(private toastCtrl: ToastController) { }
+  classInfo = {
+    'name': '',
+    'class': '',
+    'teacher': '',
+    'semester': ''
+  }
 
+  constructor(private router: Router, private httpService:CommonService, private toastCtrl: ToastController, private alertCtrl: AlertController, private localStorageService: LocalStorageService) { }
+
+  @ViewChild('joinClassSlides', { static: true }) joinClassSlides: IonSlides
   ngOnInit() {
+    this.joinClassSlides.lockSwipeToNext(true)
+    this.joinClassSlides.lockSwipeToPrev(true)
   }
 
   async findClass(){
     if(this.classNumber != ''){
-      
+      const api = '/class/info' + '?number=' + this.classNumber
+      this.httpService.ajaxGet(api).then(async (res:any)=>{
+        if(res.code == 200){
+          this.classInfo = res.data
+          this.joinClassSlides.lockSwipeToNext(false)
+          this.joinClassSlides.slideNext()
+          this.joinClassSlides.lockSwipeToNext(true)
+        }else if(res.code == 400){
+          const toast = await this.toastCtrl.create({
+            message: '你输入的班课不存在，请重新输入',
+            duration: 3000
+          })
+          toast.present()
+        }
+      }).catch((err)=>{
+        console.log(err)
+      })
     }
     else{
       const toast = await this.toastCtrl.create({
@@ -26,5 +55,33 @@ export class JoinClassPage implements OnInit {
       })
       toast.present()
     }
+  }
+  
+  async join(){
+    const api = '/class/join'
+    const json = {
+      "number" : this.classNumber,
+      "phone" : this.localStorageService.get(USER_KEY, {"phone":null}).phone
+    }
+    this.httpService.ajaxPost(api,json).then(async (res:any)=>{
+      if(res.code == 200){
+        const alert = await this.alertCtrl.create({
+          header: '提示',
+          backdropDismiss: false,
+          message: '您已加入班课 '+this.classInfo.name,
+          buttons: [{
+            text: '确定',
+            handler: () => {
+              this.router.navigateByUrl('home/class')
+            }
+          }]
+        })
+        alert.present()
+      }else if(res.code == 400){
+        console.log(res.code)
+      }
+    }).catch((err)=>{
+        console.log(err)
+    })
   }
 }
